db5903de3e1a5819151546692901bf7ec2e3287d
falanfila <138436030+falanfila@users.noreply.github.com>
2025-12-08 20:54:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var x = 0
-console.log("Hello world!")
+
 const alan = document.getElementById("mesajAlani");
 alan.textContent = "Merhaba Dünya! JavaScript ile bu yazıyı ekledim.";
 
@@ -13,12 +13,4 @@ function d() {
     document.getElementById("demo").innerHTML
     =
     x++;
-}
-
-if (x = 50) {
-    window.alert("Cat will explode! Make it vomit!")
-}
-
-if (x >= 60) {
-    window.alert("Cat exploded.")
 }
