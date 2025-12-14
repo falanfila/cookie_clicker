f541a0c731b9a47a9eb317e6640a5f5ecadde240
falanfila <138436030+falanfila@users.noreply.github.com>
2025-12-14 21:08:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var x = 0
 import Math
 
-console.log("Hello, I am Atlas! I can't see you but nice to have a user in my website!")
+console.log("Congratulations! You've found the easter egg!")
 const alan = document.getElementById("mesajAlani");
 
 function p() {
@@ -17,7 +17,16 @@ function d() {
 }
 
 function l() {
-    document.getElementById("demo").innerHTML
-    =
-    x = x + Math.floor(Math.random() * 50) + 1;
+    alert("Psst! There is an easter egg which can only be opened in a computer! A clue: try to open something from settings part. If you've found it, click the console!")
+    const eskiLog = console.log;
+
+// console.log'u override et
+console.log = function(...args) {
+    eskiLog.apply(console, args); // önce normal log
+    x += 1000000;                  // x'i 1.000.000 arttır
+    alert("Your total cookie has expanded by 1.000.000!");
+};
+
+// Test
+console.log("Congratulations! You've found the easter egg!"); 
 }
