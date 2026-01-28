b8b40d9b54c107a7c91b3787a429cfce869f89c1
DislekHamstr <138436030+falanfila@users.noreply.github.com>
2026-01-28 12:19:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,16 +14,22 @@ function rutbeKontrol() {
         z = "Baker Apprentice";
     } else if (x >= 700 && x < 1500) {
         z = "Baker";
+        alert("A new degree!")
     } else if (x >= 1500 && x < 3000) {
         z = "Cookie Fabricator";
+        alert("A new degree!")
     } else if (x >= 3000 && x < 5000) {
         z = "Master Chef";
+        alert("A new degree!")
     } else if (x >= 5000 && x < 10000) {
         z = "Cookie Riche";
+        alert("A new degree!")
     } else if (x >= 10000 && x < 20000) {
         z = "Cookie Emperor";
+        alert("A new degree!")
     } else if (x >= 20000) {
         z = "Cookie God";
+        alert("A new degree!")
     }
     document.getElementById("degree").innerHTML = z;
 }
